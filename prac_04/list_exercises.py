a29c29699a8e8daf8638a91ed2ebdd891e2db416
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-08-27 09:45:27 +1000

    Extend first list exercise program with a while loop

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -7,11 +7,12 @@ List Exercises
 
 
 def main():
-    """Program that stores 5 number inputs in a list for operation outputs"""
+    """Program that stores integer inputs in a list for operation outputs"""
     numbers = []
-    for i in range(5):
-        number = int(input("Number: "))
+    number = int(input(f"Number {len(numbers) + 1}: "))
+    while number >= 0:
         numbers.append(number)
+        number = int(input(f"Number {len(numbers) + 1}: "))
     print(f"The first number is {numbers[0]}")
     print(f"The last number is {numbers[-1]}")
     print(f"The smallest number is {min(numbers)}")
@@ -22,16 +23,17 @@ def main():
 # 2. Woefully inadequate security checker
 
 
-# def main():
-#     """Program that checks if input username is in a list of usernames"""
-#     usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
-#                  'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
-#                  'bob']
-#     username = input("Username: ")
-#     if username in usernames:
-#         print("Access granted")
-#     else:
-#         print("Access denied")
+def main2():
+    """Program that checks if input username is in a list of usernames"""
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = input("Username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 
 main()
+main2()
