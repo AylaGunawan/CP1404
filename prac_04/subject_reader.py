38c85e39e61c529583b03fe5cbd1ee2da392aa3e
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-08-26 17:09:28 +1000

    Change subject reader program to return data as a list of lists and print details in a function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,33 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_subject_details(data)
 
 
 def get_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Read and returns data from file formatted like: subject,lecturer,number of students."""
+    subjects = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+
+        subjects.append(parts)  # Add parts to subjects list
+        # print(subjects)  # See if that worked
+        # print("----------")
     input_file.close()
+    return subjects
+
+
+def print_subject_details(data):
+    """Print subject details using data"""
+    for subject in data:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
 
 
 main()
