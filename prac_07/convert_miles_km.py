59f8963d5e8401ebe18fcf6b2657ea3885290add
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-09-16 13:35:42 +1000

    Modify convert_miles_km program files to suit solutions

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -6,25 +6,44 @@ Miles to Kilometres Converter
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 __author__ = "Ayla Gunawan"
+MILES_TO_KILOMETRES_MULTIPLIER = 1.60934
 
 
 class ConvertMilesToKilometresApp(App):
+    """Kivy App that converts miles to kilometres."""
+    output_kilometres = StringProperty()
+
     def build(self):
+        """Build app from .kv file."""
         Window.size = (600, 300)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
-    def handle_convert(self):
-        print("Convert")
+    def handle_increment(self, miles, increment):
+        """Handle up/down increment for input text."""
+        incremented_miles = float(miles) + increment
+        self.root.ids.input_miles.text = str(incremented_miles)
 
-    def handle_up(self):
-        print("Up")
+    def handle_convert(self, miles):
+        """Handle conversion of miles to kilometers."""
+        kilometers = self.convert_to_number(miles) * MILES_TO_KILOMETRES_MULTIPLIER
+        self.update_result(kilometers)
 
-    def handle_down(self):
-        print("Down")
+    def update_result(self, kilometres):
+        """Update result in text output."""
+        self.output_kilometres = str(kilometres)
+
+    @staticmethod
+    def convert_to_number(string):
+        """Convert string to float or 0.0 invalid."""
+        try:
+            return float(string)
+        except ValueError:
+            return 0.0
 
 
 ConvertMilesToKilometresApp().run()
