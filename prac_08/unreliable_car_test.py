97a82627cbbb1082b61b7f8b64fbfb4db39f561f
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-09-24 14:36:42 +1000

    Add to unreliable_car_test client program

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -4,3 +4,18 @@ Unreliable Car Test
 """
 
 from prac_08.unreliable_car import UnreliableCar
+
+
+def main():  # for testing
+    """Unreliable car test client code."""
+    old_car = UnreliableCar("Old Car", 100, 50)
+    print(old_car)
+    print(f"{old_car.name} drove {old_car.drive(25)} km")
+    print(old_car)
+    old_car.add_fuel(75)
+    print(old_car)
+    print(f"{old_car.name} drove {old_car.drive(100)} km")
+    print(old_car)
+
+
+main()
