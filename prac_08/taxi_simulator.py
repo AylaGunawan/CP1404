b40017aa24008be22b3ba8421744b2a1cb40c7c0
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-10-08 14:59:51 +1000

    Modify taxi_simulator code from in-class feedback

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -12,7 +12,6 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     """Taxi simulator program."""
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
-    chosen_taxi = ""
     bill = 0
 
     print("Let's drive!")
@@ -22,22 +21,22 @@ def main():
         if choice == "c":
             print("Taxis available:")
             print_taxis(taxis)
-            chosen_taxi = get_valid_taxi_index(taxis)
+            chosen_taxi = get_taxi(taxis)
         elif choice == "d":
-            if chosen_taxi == "":
-                print("You need to choose a taxi before you can drive")
-            else:
+            if chosen_taxi:
                 drive_distance = get_valid_distance()
-                taxis[chosen_taxi].drive(drive_distance)  # DRY? taxis[chosen_taxi] and .get_fare()?
-                print(f"Your {taxis[chosen_taxi].name} trip cost you ${taxis[chosen_taxi].get_fare():.2f}")
-                bill += taxis[chosen_taxi].get_fare()
-                taxis[chosen_taxi].start_fare()
+                chosen_taxi.drive(drive_distance)
+                print(f"Your {chosen_taxi.name} trip cost you ${chosen_taxi.get_fare():.2f}")
+                bill += chosen_taxi.get_fare()
+                chosen_taxi.start_fare()
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid option")
         print(f"Bill to date: ${bill:.2f}")
         print(MENU)
         choice = input(">>> ").lower()
-    print(f"Total trip cost: ${bill:.2f}")  # DRY? print_bill function?
+    print(f"Total trip cost: ${bill:.2f}")
     print("Taxis are now:")
     print_taxis(taxis)
 
@@ -48,30 +47,29 @@ def print_taxis(taxis):
         print(i, "-", taxi)
 
 
-def get_valid_taxi_index(taxis):
-    """Get a valid taxi index within the taxis list range"""
-    is_valid_input = False
-    while not is_valid_input:
+def get_taxi(taxis):
+    """Return a chosen taxi if index within range, else return an empty string."""
+    while True:
         try:
-            chosen_taxi = int(input("Choose taxi: "))
-            if chosen_taxi > (len(taxis) - 1):
+            chosen_taxi_index = int(input("Choose taxi: "))
+            if chosen_taxi_index > len(taxis) - 1:
                 print("Invalid taxi choice")
-                chosen_taxi = ""
-            return chosen_taxi
+                return None
+            else:
+                return taxis[chosen_taxi_index]
         except ValueError:
             print("Invalid input")
 
 
 def get_valid_distance():
-    """Get a valid non-negative integer for distance."""
-    is_valid_input = False
-    while not is_valid_input:
+    """Return a valid non-negative integer for distance, else return 0."""
+    while True:
         try:
-            distance = int(input("Drive how far? "))
+            distance = int(input("Drive how far? "))  # -ve distance?
             if distance < 0:
-                print("Invalid distance")
-                distance = int(input("Drive how far? "))
-            return distance
+                return 0  # is this okay?
+            else:
+                return distance
         except ValueError:
             print("Invalid input")
 
