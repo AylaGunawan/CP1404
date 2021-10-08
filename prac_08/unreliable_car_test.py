64502eb1d007da43b3963a4a65834572651025de
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-10-08 14:08:27 +1000

    Modify unreliable_car_test code from in-class feedback

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -10,12 +10,9 @@ def main():  # for testing
     """Unreliable car test client code."""
     old_car = UnreliableCar("Old Car", 100, 50)
     print(old_car)
-    print(f"{old_car.name} drove {old_car.drive(25)} km")
-    print(old_car)
-    old_car.add_fuel(75)
-    print(old_car)
-    print(f"{old_car.name} drove {old_car.drive(100)} km")
-    print(old_car)
+    for i in range(10):
+        print(f"{old_car.name} drove {old_car.drive(25)} km")
+        print(old_car)
 
 
 main()
