737275b19475cb4749caf05d47a23554c071c25f
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-10-24 11:44:46 +1000

    Modify and test sort_files_1.py to sort files

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -3,9 +3,36 @@ CP1404 - Practical 09
 Sort Files 1
 """
 
+import shutil
+import os
+
+CURRENT_DIRECTORY = "FilesToSort"
+
 
 def main():
-    pass
+    """Program that sorts files by extension."""
+    extensions = []
+
+    os.chdir(CURRENT_DIRECTORY)
+
+    for filename in os.listdir('.'):  # when messing with files, does SRP apply?
+        if os.path.isdir(filename):
+            continue
+        extension_index = filename.find(".")
+        extension = filename[extension_index + 1:]
+        extensions.append(extension)
+    extensions = set(extensions)
+
+    for extension in extensions:
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+
+    for filename in os.listdir('.'):
+        for extension in extensions:
+            if filename.endswith(extension):
+                shutil.move(filename, extension)
 
 
 main()
