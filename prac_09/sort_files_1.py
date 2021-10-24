af8e86a15ab01ce18afcf40cb55451bb11ff7b27
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-10-24 15:56:46 +1000

    Modify sort_files_1.py and sort_files_2.py to suit solutions

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -10,29 +10,27 @@ CURRENT_DIRECTORY = "FilesToSort"
 
 
 def main():
-    """Program that makes folders to sort files by extension."""
+    """Program that makes folders to sort files by extension (Version 1)."""
     extensions = []
 
     os.chdir(CURRENT_DIRECTORY)
 
-    for filename in os.listdir('.'):  # when messing with files, does SRP apply?
+    for filename in os.listdir('.'):
         if os.path.isdir(filename):
             continue
-        extension_index = filename.find(".")
-        extension = filename[extension_index + 1:]  # extension = filename.split('.')[-1]
-        extensions.append(extension)
-    extensions = set(extensions)  # instructions say to use sets, but solutions don't?
-
-    for extension in extensions:
+        extension = filename.split('.')[-1]
         try:
             os.mkdir(extension)
         except FileExistsError:
             pass
+        if extension not in extensions:
+            extensions.append(extension)
+        # extensions = set(extensions)  instructions say to use sets, but solutions don't?
 
-    for filename in os.listdir('.'):
-        for extension in extensions:
+        for extension in extensions:  # os.rename(filename, "{}/{}".format(extension, filename))
             if filename.endswith(extension):
-                shutil.move(filename, extension)  # os.rename(filename, "{}/{}".format(extension, filename))
+                print(f"Moved {filename}.{extension} to {extension} folder")
+                shutil.move(filename, extension)
 
 
 main()
